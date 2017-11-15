cc60d3ec07e8c328bea4f7966afd9403cdf23ca6
Harvin Bryce D Almeida <harvindalmeida@gmail.com>
2017-11-15 12:37:20 -0500

    Built prod

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -3,10 +3,8 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds135364.mlab.com:35364/heroku_9rmt84hj'; // use yours
+  connectionString += '@ds163745.mlab.com:63745/heroku_cdd5zl48'; // use yours
 }
-
-
 
 var mongoose = require("mongoose");
 var db = mongoose.connect(connectionString, { useMongoClient: true});
